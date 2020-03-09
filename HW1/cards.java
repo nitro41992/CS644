7d9a56ad8bc821fdbf5251ffdee3a4b9b90633ee
nitro41992 <nick.k@iton.net>
2020-03-08 23:36:07 -0400

    Task: HBase Updates

--- HW1/cards.java
+++ HW1/cards.java
@@ -11,19 +11,49 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.filter.FirstKeyOnlyFilter;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
+import org.apache.hadoop.hbase.mapreduce.TableMapper;
+import org.apache.hadoop.hbase.mapreduce.TableReducer;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HBaseAdmin;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.TableMapper;
+import org.apache.hadoop.hbase.TableMapReduceUtil;
 
 public class cards {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Configuration config = new Configuration();
-		Job job = Job.getInstance(config, "Finding Missing Cards");
+		// Configuration config = new Configuration();
+
+		Configuration conf = HbaseConfiguration.create();
+		Connection conn = ConnectionFactory.createConnection(conf);
+		Admin admin = conn.getAdmin();
+		HTableDescriptor htdesc = new HTableDescriptor(TableName.valueOf("MissingCards"));
+		HColumnDescriptor hcdesc = new HColumnDescriptor(Bytes.toBytes("cf"));
+		htdesc.addFamily(hcdesc);
+		admin.createTable(htdesc);
+
+		Job job = Job.getInstance(conf, "Finding Missing Cards");
 
 		job.setJarByClass(cards.class);
 		job.setMapperClass(map.class);
 		job.setReducerClass(reduce.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
+		TableMapReduceUtil.initTableReducerJob("cards", reduce.class, job);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
 		FileOutputFormat.setOutputPath(job, new Path(args[1]));
@@ -43,7 +73,7 @@ public class cards {
 
 			String row = value.toString();
 			String[] subRow = row.split(",");
-			Text suit = new Text(subRow[0]);
+			Text suit = new Text(subRow[2]);
 			IntWritable intWritable = new IntWritable(Integer.parseInt(subRow[1]));
 			context.write(suit, intWritable);
 
@@ -52,6 +82,12 @@ public class cards {
 
 	// extend reducer class
 	public static class reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+
+		IntWritable result = new IntWritable();
+		public static final byte[] CF = "cf".getBytes();
+		public static final byte[] SUITE = "suite".getBytes();
+		public static final byte[] NUMBER = "number".getBytes();
+
 		public void reduce(Text key, Iterable<IntWritable> value, Context context)
 				throws IOException, InterruptedException {
 
@@ -78,11 +114,15 @@ public class cards {
 				size = deck.size();
 				int counter = 0;
 				while (counter < size) {
-					IntWritable intWritable = new IntWritable(deck.get(counter));
-					context.write(key, intWritable);
+					IntWritable result = new IntWritable(deck.get(counter));
+					context.write(key, result);
 					counter++;
 				}
 			}
+			Put put = new Put(Bytes.toBytes(key.toString()));
+			put.addColumn(CF, SUIT, Bytes.toBytes(key.toString()));
+			put.addColumn(CF, NUMBER, Bytes.toBytes(result.toString()));
+			context.write(null, put);
 		}
 	}
 
