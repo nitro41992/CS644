5c31241240f929cf911c231656500d4bc2b0ca88
nitro41992 <nick.k@iton.net>
2020-03-20 20:05:48 -0400

    Task: HW4 refactoring and adding comments

--- HW4/RelFrequency/src/main/java/RelFrequency.java
+++ HW4/RelFrequency/src/main/java/RelFrequency.java
@@ -14,7 +14,8 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 
 public class RelFrequency{
-
+	
+	// Creating a Comparable class to compare pairs and relative frequencies
 	public static class Pair implements Comparable<Pair> 
 	{
 		double Frequency;
@@ -44,73 +45,85 @@ public class RelFrequency{
 	}
 	
 	public static class Map extends Mapper<LongWritable, Text, Text, LongWritable>
-		{
-                
-                private Text wordPair = new Text();
-                private LongWritable frequency = new LongWritable(1);
+	{
+            
+            private Text wordPair = new Text();
+            private LongWritable frequency = new LongWritable(1);
 
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
-			{
+		{
 	
-			    			    
-                String line =value.toString().toLowerCase().replaceAll("[^a-z _+]", "");
-			    StringTokenizer tokenizer= new StringTokenizer(line);
-			    List<String> words = new ArrayList<String>();
-			    
-			    while(tokenizer.hasMoreElements())
-			    {
-			    	String word = new String(tokenizer.nextToken());
-			    	word = word.trim();
-			    	
-			    	if (word.length() < 3) continue;
-			    	words.add(word);
-			    	
-			    }
-			    for (int i=0;i<words.size();i++)
-				{
-					wordPair.set("*"+" "+words.get(i).trim());
-        	    	context.write(wordPair, frequency);
-				}
-				
-			    for (int i=1;i<words.size();i++)
-			    {   
-			        wordPair.set(words.get(i-1).trim()+" "+words.get(i).trim());
-			    	context.write(wordPair,frequency);
-			    	
-			    }
-
+		    // Converts all characters to lower case and uses regex to remove special characters and numbers			    
+	        String line = value.toString().toLowerCase().replaceAll("[^a-z _+]", "");
+	        
+	        // Split line into tokens based on space delimiter
+		    StringTokenizer tokenizer = new StringTokenizer(line);
+		    List<String> words = new ArrayList<String>();
+		    
+		    // Loop through all words
+		    while(tokenizer.hasMoreElements())
+		    {
+		    	// Iterating through each token within while loop
+		    	String word = new String(tokenizer.nextToken());
+		    	
+		    	// Trimming white spaces and adding word to words list
+		    	word = word.trim();
+		    	words.add(word);
+		    	
 		    }
+		    
+		    // Looping through all words
+		    for (int i = 0; i < words.size(); i++)
+			{
+		    	// Setting and writing single word pairs with * as 1st word 
+		    	wordPair.set("*"+ " " + words.get(i).trim());
+    	    	context.write(wordPair, frequency);
+			}
+			
+		    for (int i = 1; i < words.size(); i++)
+		    {   
+		    	// Setting and writing neighboring words as pairs
+		    	wordPair.set(words.get(i-1).trim() + " " + words.get(i).trim());
+		    	context.write(wordPair,frequency);
+		    	
+		    }
+			
+	
+	    }
 
-		}
+	}
 	
 	public static class Combiner extends Reducer<Text, LongWritable, Text, LongWritable>
+	{
+		private LongWritable frequency = new LongWritable();
+		
+	    public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException
 		{
-			private LongWritable frequency=new LongWritable();
+			long sum = 0;
 			
-		    public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException
+			// Loops through all frequencies based on the matching keys, sums frequency values and passes combined frequencies to the reducer
+			for (LongWritable value : values)
 			{
-				long sum = 0;
-				
-				for (LongWritable val : values)
-					{
-						sum = sum + val.get();
-					}
-				
-				frequency.set(sum);
-				
-				context.write(key, frequency);
+				sum = sum + value.get();
 			}
-
+			
+			frequency.set(sum);
+			
+			context.write(key, frequency);
 		}
+
+	}
     
+	// Establishing TreeSet to maintain a dictionary of unique key: value pairs
 	public static TreeSet<Pair> ts = new TreeSet<Pair>();
 
 	public static class Reduce extends Reducer<Text, LongWritable, Text, Text> 
-	   {
+	{
 
 		private DoubleWritable Frequency = new DoubleWritable();
-
-		private HashMap<String,Double> hashMap = new HashMap<String,Double>();
+		
+		// Establishing a HashMap to convert data into a data frame of key: value pairs
+		private HashMap<String,Double> hashMap = new HashMap<String, Double>();
 		
 
 		private double getCount(Iterable<LongWritable> values) 
@@ -121,44 +134,54 @@ public class RelFrequency{
 			{
 				count += value.get();
 			}
+			
 			return count;
+			
 		}
 
-		public void reduce(Text key, Iterable<LongWritable> values, Context context)
-			throws IOException, InterruptedException 
-			{
-
+		public void reduce(Text key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException 
+		{
+			// Extracting single words for single word frequencies based on the existence of *
 			if (key.toString().split(" ")[0].equals("*")) 
 			{
-				double pairFrequency = 0.0;
+				double wordFrequency = 0.0;
 				String word = key.toString().split(" ")[1];
-
-				pairFrequency= getCount(values);        // A word Count
-
-				hashMap.put(word,pairFrequency);
+				
+				// Getting frequency of individual words and adding to HashMap
+				wordFrequency= getCount(values);        
+				hashMap.put(word, wordFrequency);
 			}
 			else 
 			{
-				String word = key.toString().split(" ")[0];
-
-				double count = getCount(values);   
-				double wordFrequency = hashMap.get(word);
+				// Extracting first word from word pair
+				String wordA = key.toString().split(" ")[0];
 				
-				Frequency.set((double) count / wordFrequency);  
+				// Getting frequency of word pair from combiner
+				double pairFrequency = getCount(values);
+				
+				// Getting frequency of individual word from HashMap
+				double wordAFrequency = hashMap.get(wordA);
+				
+				// Calculating relative frequency of word pair
+				Frequency.set((double) pairFrequency / wordAFrequency);  
 				Double FinalFrequency = Frequency.get();
-
-					if(FinalFrequency != 1.0d)
-						{
-							ts.add(new Pair(FinalFrequency, key.toString()));
-							if (ts.size() > 100) 
-							{
-								ts.pollLast(); 
-							} 
-						}
+				
+				// Adding the top 100 word pairs and associated word pairs to TreeSet
+				if(FinalFrequency != 1.0d)
+				{
+					ts.add(new Pair(FinalFrequency, key.toString()));
+					
+					if (ts.size() > 100) 
+					{
+						ts.pollLast(); 
+					} 
+				}
 			
 			} 
 		}
-
+		
+		
+		// Collecting output and writing to output
 		protected void cleanup(Context context) throws IOException, InterruptedException 
 		{
 			while (!ts.isEmpty()) 
